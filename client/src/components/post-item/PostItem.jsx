81d94387b6a68a29dd54f0efd7f77f2d9b87a22f
Raye <brileyforbusiness@gmail.com>
2022-08-02 23:13:56 +0300

    Get post | update post added

--- client/src/components/post-item/PostItem.jsx
+++ client/src/components/post-item/PostItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
 import PostStatistic from "../UI/post-statistic/PostStatistic";
 import {MdOutlineModeEdit} from "react-icons/md";
 import {IoCloseSharp} from "react-icons/io5";
 import {useDispatch} from "react-redux";
-import {deletePost} from "../../store/reducers/postReducer";
+import {deletePost, getOnePost} from "../../store/reducers/postReducer";
 import './postItem.scss'
 
 const PostItem = ({post}) => {
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     return (
         <div>
@@ -16,16 +18,20 @@ const PostItem = ({post}) => {
                     className='post__image'
                     src="https://proprikol.ru/wp-content/uploads/2020/06/kartinki-zavtrak-35.jpg"
                     alt="post background"
+                    onClick={() => dispatch(getOnePost({post, navigate}))}
                 />
                 <div className='post__edits'>
-                    <MdOutlineModeEdit size={26}/>
+                    <MdOutlineModeEdit
+                        size={26}
+                        onClick={() => navigate('/edit')}
+                    />
                     <IoCloseSharp
                         size={26}
                         className='ml-2 text-red-700'
                         onClick={() => dispatch(deletePost(post))}
                     />
                 </div>
-                <PostStatistic/>
+                <PostStatistic post={post}/>
             </div>
             <div className='pt-2 px-2'>
                 <div className='font-medium'>
--- client/src/store/reducers/postReducer.js
+++ client/src/store/reducers/postReducer.js
@@ -30,6 +30,20 @@ export const getAllPosts = createAsyncThunk(
     }
 )
 
+export const getOnePost = createAsyncThunk(
+    'post/getOne', async (data, {rejectWithValue}) => {
+        try {
+            const {post, navigate} = data
+            const response = await axios.get(`http://localhost:5000/api/posts/${post._id}`)
+            navigate(`/posts/${post._id}`)
+
+            return response.data
+        } catch (e) {
+            return rejectWithValue(e.response.data.message)
+        }
+    }
+)
+
 export const deletePost = createAsyncThunk(
     'post/delete', async (post, {rejectWithValue}) => {
         try {
@@ -46,8 +60,28 @@ export const deletePost = createAsyncThunk(
     }
 )
 
+export const updateOnePost = createAsyncThunk(
+    'post/updateOne', async (data, {rejectWithValue}) => {
+        try {
+            const {post, title, description, tags} = data
+            const response = await axios.patch(`http://localhost:5000/api/posts/${post._id}`, {
+                title, description, tags
+            }, {
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+
+            return response.data
+        } catch (e) {
+            return rejectWithValue(e.response.data.message)
+        }
+    }
+)
+
 const initialState = {
     posts: [],
+    currentPost: {},
     isLoading: false
 }
 
@@ -71,8 +105,19 @@ const postReducer = createSlice({
             .addCase(getAllPosts.pending, (state) => {
                 state.isLoading = true
             })
+            .addCase(getOnePost.fulfilled, (state, action) => {
+                state.currentPost = action.payload
+            })
             .addCase(deletePost.fulfilled, (state, action) => {
                 state.posts = state.posts.filter(post => post._id !== action.payload._id)
+            })
+            .addCase(updateOnePost.fulfilled, (state, action) => {
+                state.isLoading = false
+                const post = state.posts.find(f => f._id === action.payload._id)
+                post.title = action.payload.title
+            })
+            .addCase(updateOnePost.pending, (state) => {
+                state.isLoading = true
             })
     }
 })
